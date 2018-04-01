ed9071ec226f1f9fd654639ab1c2ebd2f1434f10
Mike Treeves <mtreeves24@gmail.com>
2018-04-01 23:15:37 +0100

    change image gallery

--- ppp.js
+++ ppp.js
@@ -1,22 +1,21 @@
-      $(document).ready(function (event) {
-          var first_img = $('#imageContainer li:first img');
-          var imageContainer = $("#imageContainer");
-          $("#main-img").attr("src", $(first_img).attr("src"));
-          $("html").css($(first_img).attr("src") + ")");
+      // $(document).ready(function (event) {
+      //     var first_img = $('#imageContainer li:first img');
+      //     var imageContainer = $("#imageContainer");
+      //     $("#main-img").attr("src", $(first_img).attr("src"));
+      //     $("html").css($(first_img).attr("src") + ")");
 
-          $("#imageBox img").click(function () {
-              $("#main-img").attr("src", $(this).attr("src"));
-              $("html").css($(this).attr("src") + ")");
-              $("#main-img").fadeOut(0);
-              $("#main-img").fadeIn(1000);
-          });
+      //     $("#imageBox img").click(function () {
+      //         $("#main-img").attr("src", $(this).attr("src"));
+      //         // $("html").css($(this).attr("src") + ")");
+      //         $("#main-img").fadeOut(0);
+      //         $("#main-img").fadeIn(500);
+      //     });
 
-          imageContainer.find('ul').on('mousewheel', function (e, delta) {
-              this.scrollLeft -= (delta * 50);
-              e.preventDefault();
-          });
-      });
-
+      //     imageContainer.find('ul').on('mousewheel', function (e, delta) {
+      //         this.scrollLeft -= (delta * 50);
+      //         e.preventDefault();
+      //     });
+      // });
 
 
       //SMOOTH SCROLLING//
